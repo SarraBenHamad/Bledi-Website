82aab4780ba8e00dc7e29ef0e49b72d31424a2f8
Sarra Ben Hamad <benhamadsarra@gmail.com>
2024-11-01 18:56:31 +0100

    back to top button

--- script.js
+++ script.js
@@ -74,3 +74,35 @@ function hideSidebar(){
     const sidebar =document.querySelector(".sidebar");
     sidebar.style.display="none";
 }
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 50 ||
+    document.documentElement.scrollTop > 50
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+
+
+mybutton.addEventListener("click", function() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth" // Smooth scrolling effect
+    });
+  });
+  function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+  
+}
+
